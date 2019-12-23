e969d10c355a98cf204e3ed8e54fca4333200900
Blend Pajaziti <blend.pajaziti@katrori.net>
2019-12-23 18:34:00 +0100

    Refactoring

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,29 +1,56 @@
-//bootstrap
-require('bootstrap');
+import Vue from 'vue';
+import VueProgressBar from 'vue-progressbar';
+import { Form, HasError, AlertError } from 'vform';
+import { Datetime } from 'vue-datetime';
+import moment from 'moment';
+import swal from 'sweetalert2';
+import axios from 'axios';
+import Popper from 'popper.js';
+import jquery from 'jquery';
+import 'chart.js';
+import 'hchs-vue-charts';
+import 'vue-datetime/dist/vue-datetime.css'
+import 'bootstrap';
+import '@fortawesome/fontawesome-free';
 
-//VueJS
-window.Vue = require('vue');
+let Fire = new Vue();
+const toast = swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
 
-//Axios
-window.axios = require('axios');
+window.Vue = Vue;
+window.swal = swal;
+window.toast = toast;
+window.Fire = Fire;
+window.axios = axios;
+window.Popper = Popper;
+window.$ = window.jQuery = jquery;
+window.Form = Form;
 
-//PopperJS
-window.Popper = require('popper.js').default;
-
-//jQuery
-window.$ = window.jQuery = require('jquery');
-
-//admin-lte
 require('admin-lte');
 
-//Fontawesome
-require('@fortawesome/fontawesome-free');
+Vue.use(window.VueCharts);
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '10px'
+});
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+Vue.component('datetime', Datetime);
 
-//Axios prefs
+Vue.filter('upText', function(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+});
+Vue.filter('regDate', function(created){
+    return moment(created).format('Do MMMM YYYY');
+});
+Vue.filter('timeFormat', function(value){
+    return moment(value).format('HH:mm');
+});
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-let token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.head.querySelector('meta[name="csrf-token"]');
---
+++ resources/js/vue/router.js
@@ -0,0 +1,63 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Dashboard from './components/Dashboard';
+import Teams from './components/Teams';
+import Invoice from './components/Invoice';
+import Sponsoring from './components/Sponsoring';
+import Membership from './components/Membership';
+import Contracts from './components/Contracts';
+import Communication from './components/Communication';
+import Users from './components/Users';
+import Statistics from './components/Statistics';
+import Roles from './components/Roles';
+import Permissions from './components/Permissions';
+import Category from './components/Category';
+import Team from './components/Team';
+import TeamMembers from './components/TeamMembers';
+import Events from './components/Events';
+import Meetings from './components/Meetings';
+import Trainings from './components/Trainings';
+import Coaches from './components/Coaches';
+import Coach from './components/Coach';
+import Matchdays from './components/Matchdays';
+import Players from './components/Players';
+import Player from './components/Player';
+import Tasks from './components/Tasks';
+import Refrees from './components/Refrees';
+import FieldWardrobe from './components/FieldWardrobe';
+
+const routes = [
+    { path: '/dashboard', component: Dashboard },
+    { path: '/teams', component: Teams },
+    { path: '/invoice', component: Invoice },
+    { path: '/sponsoring', component: Sponsoring },
+    { path: '/membership', component: Membership },
+    { path: '/contracts', component: Contracts },
+    { path: '/communications', component: Communication },
+    { path: '/users', component: Users },
+    { path: '/statistics', component: Statistics },
+    { path: '/roles', component: Roles },
+    { path: '/permissions', component: Permissions },
+    { path: '/category', component: Category },
+    { path: '/team/:teamId', component: Team, props: true},
+    { path: '/members', component: TeamMembers },
+    { path: '/events', component: Events },
+    { path: '/meetings', component: Meetings },
+    { path: '/trainings', component: Trainings },
+    { path: '/coaches', component: Coaches },
+    { path: '/coach', component: Coach },
+    { path: '/matchdays', component: Matchdays },
+    { path: '/players', component: Players },
+    { path: '/player', component: Player },
+    { path: '/tasks', component: Tasks },
+    { path: '/refrees', component: Refrees },
+    { path: '/fieldwardrobe', component: FieldWardrobe },
+];
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes,
+    mode: 'history',
+    // linkActiveClass: 'active'
+})
